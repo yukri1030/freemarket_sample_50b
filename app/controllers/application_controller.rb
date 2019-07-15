edf80cd72afb9800b2bab87b3f2851284f4880b7
yikezawa <y.ikezawa93@gmail.com>
2019-07-15 15:22:16 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-
   def production?
     Rails.env.production?
   end
 
-  user = Rails.application.credentials.basic_auth[:user]
-  pw = Rails.application.credentials.basic_auth[:password]
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == user && password == pw
+      username == 'nika' && password == '4472'
     end
   end
+end
 end
