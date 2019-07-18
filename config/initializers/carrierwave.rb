d8b21923dcde47b70f3b7a8c3920c4ef9c623c2e
yukri1030 <sky.moon.go@gmail.com>
2019-07-18 14:08:18 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,11 +13,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
     region: 'ap-northeast-1'
   }
-<<<<<<< Updated upstream
-
-=======
   
->>>>>>> Stashed changes
     config.fog_directory  = 'freemarket-sample-50b'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-50b'
   end
