ed83f9a24a1323679409dd8d0c0d4df74e258db2
Takaki <takaki55730317@gmail.com>
2019-07-22 19:54:32 +0900

    create_tableの方へadd_nicknameを統合

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_050657) do
+ActiveRecord::Schema.define(version: 2019_07_22_044842) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -20,7 +21,6 @@ ActiveRecord::Schema.define(version: 2019_07_22_050657) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
