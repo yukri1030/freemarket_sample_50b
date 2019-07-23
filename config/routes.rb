c76e8807d17508ee667bd288ce995496898e6499
yukri1030 <sky.moon.go@gmail.com>
2019-07-23 21:51:06 +0900

    会員完了画面の実装
    # WHAT
    会員完了画面の実装
    
    # WHY
    新規会員登録の際に必要

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
    }
+   get 'signup/successful' , to: 'signups#successful'
   root "products#index"
-
   resources :purchases, only: [:new]
   resources :products, only: [:index, :new, :edit, :show]
   resources :users , only: [:new, :show, :create]
+  resources :cards, only: [:index, :new, :edit, :show]
   resources :user_profiles, only: [:new, :create, :edit, :update]
   resources :signups, only: [:index, :show]
-end
+ end
