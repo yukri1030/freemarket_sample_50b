e13659ae1954218b4f6b06a83073d68fe92a35cc
Takaki <takaki55730317@gmail.com>
2019-07-30 13:03:10 +0900

    validationエラーでの条件分岐に修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,5 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-    layout 'application-off-header-footer'
+  layout 'application-off-header-footer'
 
   def new
     @user = User.new
@@ -14,59 +10,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    if User.profile_nested_with_user_is_valid?(params)&&verify_recaptcha(model: resource)
+    if User.profile_nested_with_user_is_valid?(params)
       super and return
     else
-      @user =　build_resource(sign_up_params)
+      build_resource(sign_up_params)
       @profile = @user.profile
       @errors='未記入箇所があります'
       render 'new' and return
     end
- end
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
   def after_sign_up_path_for(resource)
     signup_sms_confirmation_path
   end
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  private
+
+  def check_captcha
+    self.resource = resource_class.new sign_up_params
+    resource.validate
+    unless verify_recaptcha(model: resource)
+      render :new
+    end
+  end
 end
