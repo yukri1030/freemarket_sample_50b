a8c0efedfd80ee8e738c4f172512f60e15e6b20a
Takaki <takaki55730317@gmail.com>
2019-08-04 19:18:50 +0900

    delete other active hashes.rb

--- app/models/delivery_time.rb
+++
@@ -1,9 +0,0 @@
-class DeliveryTime < ActiveHash::Base
-  field :delivery_time
-  create id: 0, delivery_time: '---'
-  create id: 1, delivery_time: '1~2日で発送'
-  create id: 2, delivery_time: '2~3日で発送'
-  create id: 3, delivery_time: '4~7日で発送'
-
-  # has_many :product
-end
