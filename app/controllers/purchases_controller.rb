d25f934a32843e68c9ea9adbcc5e8517d47d3f3b
Takaki <takaki55730317@gmail.com>
2019-08-07 01:29:10 +0900

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,65 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_product, only: [:new, :pay, :address, :card, :card_index]
+
+  require 'payjp'
+  require 'date'
+
   def new
+    @card = current_user.card
+    if @card
+      Payjp.api_key = Rails.application.credentials.payjp[:test_secret_key]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
     render layout: 'application-off-header-footer.html.haml'
+  end
+
+  def pay
+    card = Card.find_by(user_id: current_user.id
+    deal = Deal.find_by(product_id: @product.id)
+    if card
+      Payjp.api_key = Rails.application.credentials.payjp[:test_secret_key]
+      charge = Payjp::Charge.create(
+        amount: @product.price,
+        customer: card.customer_id,
+        currency: 'jpy'
+      )
+      deal.charge_id = charge.id
+      deal.buyer_id = current_user.id
+      deal.status_id = 2 
+      deal.deal_at = DateTime.now
+      deal.save
+      flash[:notice] = '商品を購入しました'
+      redirect_to controller: :users, action: :show, id: current_user.id
+    else
+      flash[:alert] = 'カード情報を登録してください'
+      redirect_to controller: :purchases, action: :new, product_id: @product.id
+    end
+  end
+
+  def address
+    render layout: 'application-off-header-footer.html.haml'
+  end
+
+  def card
+    gon.payjp_test_pk = Rails.application.credentials.payjp[:test_public_key]
+    render layout: 'appliction-off-header-footer.html.haml'
+  end
+
+  def card_index
+    @card = current_user.card
+    if @card
+      Payjp.api_key = Rails.application.credentials.payjp[:test_secret_key]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
+    render layout: 'application-off-header-footer.html.haml'
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:product_id])
   end
 end
