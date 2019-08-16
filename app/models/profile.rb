b0c40b1edcdd8f8014561f8c52e4e4ae2c0592bb
Takaki <takaki55730317@gmail.com>
2019-08-17 01:18:39 +0900

    fixed error omni

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,11 +3,6 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   validates :phone_number, presence: true, on: :sms_confirmation_send
-  validates :family_name, presence: true
-  validates :last_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth_ymd, presence: true
   with_options presence: true,on: :hoge  do
     validates  :zipcode, :address_prefecture, :address_city, :address_street_number
   end
