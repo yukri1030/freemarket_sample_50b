62007d84de03befd021381d48a0a224398c39380
Abe Yukari <sakura@AbenoMacBook-Air.local>
2019-08-16 16:51:14 +0900

    ユーザー本人確認情報のサーバーサイドの実装
    # WHAT
    プロフィールの本人確認情報のサーバーサイドの実装

--- app/controllers/user_confirmations_controller.rb
+++ app/controllers/user_confirmations_controller.rb
@@ -1,10 +1,16 @@
 class UserConfirmationsController < ApplicationController
   def edit
-    # @user = User.find(paramd[:user_id]).profile
+    @user = User.find(params[:user_id]).profile
   end
 
-  # private
-  # def profile_params
-  #   params.require(:profile).permit(:zipcode, :address_prefecture, :address_city, :address_street_number, :address_building_name)
-  # end
+  def update
+    @user = User.find(params[:user_id]).profile
+    @user.update(profile_params)
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:zipcode, :address_prefecture, :address_city, :address_street_number, :address_building_name)
+  end
 end
