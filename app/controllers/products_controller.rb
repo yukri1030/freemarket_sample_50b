1e0c2fc98449b771b670cec8f636a283071855cd
Takaki <takaki55730317@gmail.com>
2019-08-20 22:26:15 +0900

    サーバーサイド-出品商品機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -37,7 +37,70 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    render layout: 'application-off-header-footer.html.haml'
+    @product = Product.find(params[:id])
+    gon.product = @product
+    gon.product_images = @product.product_images
+
+    # @product.product_imagse.image_urlをバイナリーデータにしてビューで表示できるようにする
+    require 'base64'
+    # require 'aws-sdk'
+
+    gon.product_images_binary_datas = []
+    if Rails.env.production?
+      client = Aws::S3::Client.new(
+                             region: 'ap-northeast-1',
+                             access_key_id: Rails.application.credentials.aws[:access_key_id],
+                             secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+                             )
+      @product.product_images.each do |image|
+        binary_data = client.get_object(bucket: 'freemarket-taka50b', key: image.image_url.file.path).body.read
+        gon.product_images_binary_datas << Base64.strict_encode64(binary_data)
+      end
+    else
+      @product.product_images.each do |image|
+        binary_data = File.read(image.image_url.file.file)
+        gon.product_images_binary_datas << Base64.strict_encode64(binary_data)
+      end
+    end
+  end
+
+  def update
+    # ブランド名がstringでparamsに入ってくるので、id番号に書き換え
+    if  brand = Brand.find_by(name: params[:product][:brand_id])
+      params[:product][:brand_id] = brand.id
+    else
+      params[:product][:brand_id] = Brand.create(name: params[:product][:brand_id]).id
+    end
+
+    @product = Product.find(params[:id])
+
+    # 登録済画像のidの配列を生成
+    ids = @product.product_images.map{|image| image.id }
+    # 登録済画像のうち、編集後もまだ残っている画像のidの配列を生成(文字列から数値に変換)
+    exist_ids = registered_image_params[:ids].map(&:to_i)
+    # 登録済画像が残っていない場合(配列に０が格納されている)、配列を空にする
+    exist_ids.clear if exist_ids[0] == 0
+    if (exist_ids.length != 0 || new_image_params[:images][0] != " ") && @product.update(product_params)
+      # 登録済画像のうち削除ボタンをおした画像を削除
+      unless ids.length == exist_ids.length
+        # 削除する画像のidの配列を生成
+        delete_ids = ids - exist_ids
+        delete_ids.each do |id|
+          @product.product_images.find(id).destroy
+        end
+      end
+      # 新規登録画像があればcreate
+      unless new_image_params[:images][0] == " "
+        new_image_params[:images].each do |image|
+          @product.product_images.create(image_url: image, product_id: @product.id)
+        end
+      end
+      flash[:notice] = '編集が完了しました'
+      redirect_to product_path(@product), data: {turbolinks: false}
+    else
+      flash[:alert] = '未入力項目があります'
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
@@ -73,6 +136,7 @@ class ProductsController < ApplicationController
   end
 
   private
+
   def set_product
     @product = Product.find(params[:id])
   end
