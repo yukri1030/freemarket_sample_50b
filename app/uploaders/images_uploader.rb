a96b57c80ed7e3440790d49280874e67877e53ab
Takaki <takaki55730317@gmail.com>
2019-09-11 12:50:32 +0900

    basic認証のusenameとpasswordを環境変数で設定

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -1,7 +1,7 @@
 class ImagesUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -37,7 +37,7 @@ class ImagesUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [200, 200]
+  #   process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
